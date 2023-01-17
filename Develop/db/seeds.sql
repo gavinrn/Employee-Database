7920a85fffb35264b48de16991d21280717899e7
Gavin McNutt <gavinmcnutt1@gmail.com>
2023-01-17 00:40:59 -0800

    solid foundations for inquirer, and finished creating database

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,15 +1,21 @@
 INSERT INTO department (department_name)
-VALUES  ("CUSTOMER SERVICE")
-        ("FRONT-END ENGINEER")
-        ("BACK-END ENGINEER")
-        ("LEAD ENGINEER")
-        ("FOUNDER")
+VALUES  ("CUSTOMER SERVICE"),
+        ("FRONT-END ENGINEER"),
+        ("BACK-END ENGINEER"),
+        ("LEAD ENGINEER"),
+        ("FOUNDER");
 
-INSERT INTO role (title ,salary)
-VALUES  ("CODE TESTER" , 75000),
-        ("CODE WRITER" , 90000),
-        ("CODING PARTNER" , 90000),
-        ("DATABASE MANAGER" , 100000),
-        ("STYLE ARTIST" , 65000);
+INSERT INTO role (title ,salary , department_id)
+VALUES  ("CODE TESTER" , 75000 , 2),
+        ("CODE WRITER" , 90000 , 3),
+        ("CODING PARTNER" , 90000 , 5),
+        ("DATABASE MANAGER" , 100000, 5),
+        ("STYLE ARTIST" , 65000, 1);
+
+INSERT INTO employee (first_name , last_name , role_id , manager_id)
+VALUES  ("John" , "Foreman" , 5 , null),
+	    ("Gavin" , "Walance" , 1 , 2),
+	    ("Steven" , "Lee" , 5 , 2),
+	    ("Jamal" , "Lou" , 2 , 4);
 
 
--- Develop/index.js
+++ Develop/index.js
@@ -1,35 +1,47 @@
 const inquirer = require('inquirer');
 const MySQL = require('mysql2');
 
-var running = true;
-//while (running) {
-  inquirer
-    .prompt([
-      // {
-      //   type: 'input',
-      //   name: 'name',
-      //   message: 'What is your name?',
-      // },
-      // {
-      //   type: 'checkbox',
-      //   message: 'What languages do you know?',
-      //   name: 'stack',
-      //   choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-      // },
-      {
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'contact',
-        choices: ['email', 'phone', 'telekinesis'],
-      },
-      // {
-      //   type: 'confirm',
-      //   message: 'Quit',
-      //   name: 'quit',
 
-      // }
-    ])
-    .then((data) => {
-      console.log(data);
-    });
-  //}
+const questions = [
+  {
+    type: 'list',
+    name: 'main',
+    message: 'What would you like to do?',
+    choices: ["view all departments" , "view all roles" , "view all employees" , "add a department" , "add a role" , "add an employee", "update an employee" , "Quit"]
+
+  },
+  {
+    type: 'input',
+    name: 'view all departments',
+    when: function(answers) {
+      return answers.main === "view all departments";
+    },
+    message: "You are viewing all departments"
+  }
+
+
+];
+
+function ask() {
+  inquirer.prompt(questions).then((answers) => {
+
+    if (answers.main === "view all roles") {
+      console.log("yea baby");
+    }
+
+
+
+
+
+    if (answers.main === "Quit") {
+      console.log('sucess');
+    } else {
+      ask();
+    }
+
+
+  });
+}
+
+ask();
+
